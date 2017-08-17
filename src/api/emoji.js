27336bae07fae9ab5c7d59327026bcc81a7cef21
Joel <inscriptions@blootips.fr>
2017-08-18 01:23:34 +0200

    Add module emoji

---
+++ src/api/emoji.js
@@ -0,0 +1,20 @@
+
+const emoji = require('node-emoji')
+
+// Return a random emoji from a keyword
+const randomEmojiFromKeyword = (keyword) => {
+
+  let output
+  /**
+      @TODO Constituer une base de données de mot clé => emoji aléatoire :
+      Un fichier txt par theme ? vacances.txt :
+      "vacances, plage, soleil, beach : emoji1, emoji2, emoji3, ..."
+  */
+  if(keyword == 'vacances'){
+    output = emoji.get('sunglasses');
+  }
+
+  return output
+}
+
+module.exports = randomEmojiFromKeyword
--- src/api/replyToKeywords.js
+++ src/api/replyToKeywords.js
@@ -1,14 +1,12 @@
 const Twit = require('twit')
 const unique = require('unique-random-array')
 const config = require('../config')
+const randomEmojiFromKeyword = require('./emoji')
 
 const param = config.twitterConfig
 const randomReply = unique(param.randomReply.split('|'))
 
 const bot = new Twit(config.twitterKeys)
-
-const emoji = require('node-emoji')
-
 
 
 // function: replies to a user from a keyword
@@ -39,11 +37,17 @@ console.log(keyword);
         }
 
         const user = data.statuses[random].user
+        const emojiReply = randomEmojiFromKeyword(keyword)
+
+        if(!emojiReply){
+          console.log('No emoji found')
+          return
+        }
 
         //console.log(data.statuses[random])
         //console.log(user)
 
-        const response = '@' + user.screen_name + ' '+ emoji.get('sunglasses')
+        const response = '@' + user.screen_name + ' '+ emojiReply
         //console.log(response);
         console.log( tweetId);
         bot.post(
